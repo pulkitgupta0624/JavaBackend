008363acbf2df30e77b237169bfa696386ff0c6e
pulkitgupta0624 <pulkitgupta2403@gmail.com>
2026-04-10 14:03:05 +0530

    new add

---
+++ springBootFirst/src/test/java/puki/demo/springbootfirst/SpringBootFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package puki.demo.springbootfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
